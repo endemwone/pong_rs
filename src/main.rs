513b0a9515b7ebdc3c52bdac0c4ad96ea29d4df2
Aadel Abdul Sammad <endemwone@gmail.com>
2023-08-16 19:28:12 +0530

    move boards

--- src/main.rs
+++ src/main.rs
@@ -34,6 +34,7 @@ struct Board {
 impl Board {
     const BOARD_HEIGHT: f32 = 50.0;
     const BOARD_WIDTH: f32 = 15.0;
+    const BOARD_MOVE_SPEED: f32 = 5.0;
 
     fn new(y: f32, board_type: BoardType) -> Self {
         Self { y, board_type }
@@ -81,32 +82,30 @@ impl Assets {
 }
 
 struct MainState {
-    board_1: Board,
-    board_2: Board,
+    board_left: Board,
+    board_right: Board,
     assets: Assets,
 }
 
 impl MainState {
     fn new(ctx: &mut Context) -> GameResult<Self> {
-        let board_1 = Board::new(SCREEN_HEIGHT / 2.0, BoardType::Left);
-        let board_2 = Board::new(SCREEN_HEIGHT / 2.0, BoardType::Right);
+        let board_left = Board::new(SCREEN_HEIGHT / 2.0, BoardType::Left);
+        let board_right = Board::new(SCREEN_HEIGHT / 2.0, BoardType::Right);
         let assets = Assets::new(ctx)?;
 
         Ok(Self {
-            board_1,
-            board_2,
+            board_left,
+            board_right,
             assets,
         })
     }
 }
 
 impl ggez::event::EventHandler<GameError> for MainState {
-    fn update(&mut self, ctx: &mut Context) -> GameResult {
-        const DESIRED_FPS: u32 = 60;
+    fn update(&mut self, _ctx: &mut Context) -> GameResult {
+        // const DESIRED_FPS: u32 = 60;
 
-        while ctx.time.check_update_time(DESIRED_FPS) {
-            println!("Time")
-        }
+        // while ctx.time.check_update_time(DESIRED_FPS) {}
 
         Ok(())
     }
@@ -114,12 +113,38 @@ impl ggez::event::EventHandler<GameError> for MainState {
     fn draw(&mut self, ctx: &mut Context) -> GameResult {
         let mut canvas = Canvas::from_frame(ctx, Color::BLACK);
 
-        self.board_1.draw(&mut canvas, &self.assets)?;
-        self.board_2.draw(&mut canvas, &self.assets)?;
+        self.board_left.draw(&mut canvas, &self.assets)?;
+        self.board_right.draw(&mut canvas, &self.assets)?;
 
         canvas.draw(&self.assets.mid_line, graphics::DrawParam::new());
 
         canvas.finish(ctx)?;
+
+        Ok(())
+    }
+
+    fn key_down_event(
+        &mut self,
+        ctx: &mut Context,
+        input: KeyInput,
+        _repeated: bool,
+    ) -> GameResult {
+        match input.keycode {
+            Some(KeyCode::W) => {
+                self.board_left.y -= Board::BOARD_MOVE_SPEED;
+            }
+            Some(KeyCode::S) => {
+                self.board_left.y += Board::BOARD_MOVE_SPEED;
+            }
+            Some(KeyCode::Up) => {
+                self.board_right.y -= Board::BOARD_MOVE_SPEED;
+            }
+            Some(KeyCode::Down) => {
+                self.board_right.y += Board::BOARD_MOVE_SPEED;
+            }
+            Some(KeyCode::Escape) => ctx.request_quit(),
+            _ => (),
+        }
 
         Ok(())
     }
